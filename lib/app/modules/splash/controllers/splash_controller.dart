42105a7e1b33d11d04eb0ed7c09a45998908ba4c
favjiw <mfavianj@gmail.com>
2025-11-20 11:33:51 +0700

    feat: slice splash

---
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class SplashController extends GetxController {
+  @override
+  void onReady() {
+    super.onReady();
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.offNamed('/home');
+    });
+  }
+}
---
+++ lib/app/modules/splash/views/splash_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/splash_controller.dart';
+
+class SplashView extends GetView<SplashController> {
+  const SplashView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/splash_img.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
